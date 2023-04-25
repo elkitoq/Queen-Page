56db76e861b619e565489ed9107b3c7199d6c40d
Victor Escudero <60633486+elkitoq@users.noreply.github.com>
2023-04-25 17:03:17 -0300

    add discography page

--- src/pages/discography/index.tsx
+++ src/pages/discography/index.tsx
@@ -1,12 +1,25 @@
-import { AnimationIntoView } from '@/components/animation-into-view'
-import CarAnimation from '@/components/travel'
-import { Container, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Disc } from '@/components/disc'
+import { PlayControls } from '@/components/player-controls'
+import { Container } from '@chakra-ui/react'
+import React, { useContext } from 'react'
+import { DiscContext } from 'src/tools/Disc-Context'
+import { Discs } from 'src/tools/discs'
+
+import dynamic from 'next/dynamic';
+
+const CarAnimation = dynamic(() => import('@/components/travel'), {
+  ssr: false
+});
 
 export default function DiscographyPage(){
+
+  const discContext = useContext(DiscContext)
+
   return (
-    <Container maxWidth={'full'} padding={0}>
-      <CarAnimation screenWidth={1290} screenHeight={1080}/>
+    <Container maxWidth={'full'} padding={0} position={'relative'} overflow={'hidden'}>
+      <CarAnimation />
+      {discContext.isPlaying && <Disc imgPath={Discs[discContext.position].imgPath} songs={Discs[discContext.position].songs} title={Discs[discContext.position].title} date={Discs[discContext.position].date}/>}
+      <PlayControls/>
     </Container>
   )
 }
